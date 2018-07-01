3a64d53567ee2bdb0e0a45f503728380a65ec846
Alexandru Olari <alexandruolari@rentalcars.com>
2018-07-01 02:51:38 +0100

    2.3.1 - Moved playlist function from app to playlist ctrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -148,34 +148,7 @@ export class AppComponent implements OnInit {
   }
 
   addPlaylistItem(i: number, list: number) {
-      let listType;
-      if (list === 0) {
-        listType = this.globals.feedVideos[i];
-      }
-      if (list === 1) {
-        listType = this.globals.lastSearchedVideos[i];
-      }
-      if (list === 2) {
-        listType = this.globals.relatedVideos[i];
-      }
-      if (list === 3) {
-        listType = this.globals.currentVideo;
-      }
-      if (list === 4) {
-        listType = this.globals.historyVideos[i];
-      }
-
-      const playlistItem = this.globals.playlistVideos.find(item => item.id === listType.id);
-
-      if (typeof playlistItem === 'undefined') {
-        this.globals.playlistVideos.push(listType);
-        this.shared.checkPlaylist();
-
-        this.shared.triggerNotify('Added to playlist');
-        this.scrollToBottom();
-      } else {
-        this.shared.triggerNotify('Video is already in playlist');
-      }
+    this.playlistCTRL.addPlaylistItem(i, list, this.myScrollContainer);
   }
 
   clearSession() {
@@ -304,17 +277,6 @@ export class AppComponent implements OnInit {
     document.execCommand('copy');
     this.videoItemIDvalue.nativeElement.blur();
     this.shared.copyShareLink();
-  }
-
-  scrollToBottom() {
-      try {
-        setTimeout( () => {
-          this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
-        }, 200);
-      } catch (err) {
-        console.log(err);
-        console.log('scroll issue');
-      }
   }
 
   // @HostListener('window:beforeunload')
--- src/app/services/playlist-control.service.ts
+++ src/app/services/playlist-control.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef } from '@angular/core';
 import { SharedService } from '../services/shared.service';
 import { GlobalsService } from '../services/globals.service';
 
@@ -29,6 +29,48 @@ export class PlaylistControlService {
       this.shared.getPlaylist();
     }
     this.shared.findPlaylistItem();
+  }
+
+  addPlaylistItem(i: number, list: number, div?: ElementRef) {
+    let listType;
+    if (list === 0) {
+      listType = this.globals.feedVideos[i];
+    }
+    if (list === 1) {
+      listType = this.globals.lastSearchedVideos[i];
+    }
+    if (list === 2) {
+      listType = this.globals.relatedVideos[i];
+    }
+    if (list === 3) {
+      listType = this.globals.currentVideo;
+    }
+    if (list === 4) {
+      listType = this.globals.historyVideos[i];
+    }
+
+    const playlistItem = this.globals.playlistVideos.find(item => item.id === listType.id);
+
+    if (typeof playlistItem === 'undefined') {
+      this.globals.playlistVideos.push(listType);
+      this.shared.checkPlaylist();
+
+      this.shared.triggerNotify('Added to playlist');
+      this.scrollToBottom(div);
+    } else {
+      this.shared.triggerNotify('Video is already in playlist');
+    }
+  }
+
+  scrollToBottom(div: ElementRef) {
+    try {
+      setTimeout( () => {
+        div.nativeElement.scrollTop = div.nativeElement.scrollHeight;
+      }, 200);
+    } catch (err) {
+      console.log(err);
+      console.log('scroll issue');
+    }
   }
 
 }
