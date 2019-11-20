b9713991cf5b633e32ce98ef490ffb94b4924004
Alexandru Olari <alexandruolari@rentalcars.com>
2019-11-20 13:16:45 +0000

    2.6.1 - Enable mini player only on homepage

--- src/app/components/player-mini/player-mini.component.ts
+++ src/app/components/player-mini/player-mini.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { GlobalsService } from '../../services/globals.service';
 import { PlayerComponent } from '../player/player.component';
 import { ButtonsComponent } from '../player/buttons/buttons.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-player-mini',
@@ -11,10 +13,18 @@ import { ButtonsComponent } from '../player/buttons/buttons.component';
 })
 export class PlayerMiniComponent implements OnInit {
 	setPlayerVisible = false;
+	disablePlayer = false;
 
-	constructor(public globals: GlobalsService, public player: PlayerComponent, public playerCTA: ButtonsComponent) { }
+	constructor(public globals: GlobalsService, public player: PlayerComponent, public playerCTA: ButtonsComponent, private router: Router) { }
 
 	ngOnInit() {
+		this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((val: NavigationEnd) => {
+			if (val.url !== '/home') {
+				this.disablePlayer = true;
+			} else {
+				this.disablePlayer = false;
+			}
+		});
 	}
 
 	@HostListener('window:scroll', ['$event'])
